8bcfc7cf9635189d8d63081ebd960b6ef762a7a2
klorn <hlm690@hotmail.com>
2020-02-02 19:32:45 +0100

    added home and app components

---
+++ src/layout/header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import style from './header.module.scss';
+
+const Header = ({}) => {
+
+  return (
+    <header>
+      <div>
+        Header
+      </div>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/pages/home/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from 'layout/header';
+
+import styles from './home.module.scss';
+
+const Home = () => {
+  return (
+    <>
+    <Header />
+
+    <div>
+      Content
+    </div>
+    </>
+  )
+}
+
+export default Home;
